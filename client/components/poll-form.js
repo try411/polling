d9c49a9705bc02c5dc1d52dc42fe3e5786173c62
try411 <kaelynmh@hawaii.edu>
2015-10-01 11:22:30 -1000

    IC11
    ICS 415-002 kaelynmh

---
+++ client/components/poll-form.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Kaelyn on 10/1/2015.
+ */
+Template.pollForm.events({
+
+  // handle the form submission
+  'submit form': function(event) {
+
+    // stop the form from submitting
+    event.preventDefault();
+
+    // get the data we need from the form
+    var newPoll = {
+      question: event.target.question.value,
+      choices: [
+        {  text: event.target.choice1.value, votes: 0 },
+        {  text: event.target.choice2.value, votes: 0 },
+        {  text: event.target.choice3.value, votes: 0 }
+      ]
+    };
+
+    // create the new poll
+    Polls.insert(newPoll);
+  }
+
+});
---
+++ collections/polls.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Kaelyn on 10/1/2015.
+ */
+Polls = new Mongo.Collection('polls');
